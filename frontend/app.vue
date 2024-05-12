f7fcd9ba997007c9ada1ea8f370534c80f341001
rithakith <ritharaedirisinghe@gmail.com>
2024-05-12 19:08:47 +0530

    get data from backend

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>{{ name }}</h1>
+    <div v-if="pending">Loading</div>
+    <ul>
+      <div v-for="project in projects" :key="project.name">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+      </div>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const name = "Rithara Kithmanthie";
+const {
+  data: projects,
+  pending,
+  error,
+} = useFetch("http://localhost:4000/projects");
+</script>
